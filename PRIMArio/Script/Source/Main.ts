f8e1659a5d1042c15848dfb0cf6e7141ec218d25
Florian Malitschenko <florian.malitschenko@outlook.de>
2022-10-19 22:17:37 +0200

    added action hander, walk / jump animations

--- PRIMArio/Script/Source/Main.ts
+++ PRIMArio/Script/Source/Main.ts
@@ -4,94 +4,156 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
-  let marioPos : ƒ.Node;
-  let marioSpriteNode : ƒAid.NodeSprite;
-  let walkSpeed: number = 1.5;
 
-//loader
+  //Mario
+  let marioCharacter: ƒ.Node;
+  let marioSpriteNode: ƒAid.NodeSprite;
+  let marioSpeed: number = 2.5;
+  let directionRight: boolean = true;
+  let isWalking: boolean = false;
 
-  document.addEventListener("interactiveViewportStarted", <EventListener><unknown>start);
+  //loader
 
+  document.addEventListener(
+    "interactiveViewportStarted",
+    <EventListener>(<unknown>start)
+  );
 
-  async function start(_event: CustomEvent): Promise <void> {
+  let animIdle: ƒAid.SpriteSheetAnimation;
+  let animJump: ƒAid.SpriteSheetAnimation;
+  let animWalkRight: ƒAid.SpriteSheetAnimation;
+
+  async function start(_event: CustomEvent): Promise<void> {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-     // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-     console.log(viewport);
-     let branch: ƒ.Node = viewport.getBranch();
-     console.log(branch);
-     marioPos = branch.getChildrenByName("Mario")[0];
-     marioSpriteNode = await createWalkRightAnimation();
-     marioPos.addChild(marioSpriteNode);
-     marioPos.getComponent(ƒ.ComponentMaterial).activate(false);
-     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-     ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
-     console.log(marioPos);
-    ƒ.Loop.start;
 
-}
+    // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start;
+    console.log(viewport);
+    let branch: ƒ.Node = viewport.getBranch();
+
+    console.log(branch);
+    marioCharacter = branch.getChildrenByName("Mario")[0];
+    marioCharacter.getComponent(ƒ.ComponentMaterial).activate(false);
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
+    console.log(marioCharacter);
+
+    actionHandler(_event);
+  }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
-    viewport.draw();
-    ƒ.AudioManager.default.update();
+    // ƒ.AudioManager.default.update();
+    // ƒ.AudioManager.default.update();
     console.log("Update");
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D ]))
-    marioPos.mtxLocal.translateX(walkSpeed*ƒ.Loop.timeFrameGame/1000);
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SHIFT_LEFT])){ 
+      marioSpeed = 10;
+      marioSpriteNode.framerate = 30;
+    }
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+      if (!isWalking){
+        marioSpriteNode.setAnimation(animWalkRight);
+        isWalking = true;   
+      }   
+      marioCharacter.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(ƒ.Loop.timeFrameGame/1000 * marioSpeed);
+      if (!directionRight) {
+        marioSpriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+        directionRight = true;
+      }
+    }
+    else if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+      if (!isWalking){
+        marioSpriteNode.setAnimation(animWalkRight);
+        isWalking = true;   
+      }
+      marioCharacter.mtxLocal.translateX(-ƒ.Loop.timeFrameGame/1000 * marioSpeed);
+      if (directionRight) {
+        marioSpriteNode.getComponent(ƒ.ComponentTransform).mtxLocal.rotateY(180);
+        directionRight = false;
+      }
+    }
+    else {
+      isWalking = false;
+      marioSpriteNode.setAnimation(animIdle);
+    }
+    if (
+      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+{
+  marioSpriteNode.setAnimation(animJump);
+  marioCharacter.getComponent(ƒ.ComponentTransform).mtxLocal.translateY(ƒ.Loop.timeFrameGame/1000 * marioSpeed);
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A ]))
-    marioPos.mtxLocal.translateX(- walkSpeed*ƒ.Loop.timeFrameGame/1000);
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W ]))
-    marioPos.mtxLocal.translateY(walkSpeed*ƒ.Loop.timeFrameGame/1000);
-  
-if (ƒ.Keyboard.isPressedCombo([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.SHIFT_LEFT ]))
-marioPos.mtxLocal.translateX(2*walkSpeed*ƒ.Loop.timeFrameGame/1000)
+
+}
+
+    if (
+      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]))
+{
+  marioSpriteNode.setAnimation(animJump);
+  marioCharacter.getComponent(ƒ.ComponentTransform).mtxLocal.translateY(ƒ.Loop.timeFrameGame/1000 * marioSpeed);
+
+
+}
+if (
+  ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]))
+{
+marioSpriteNode.setAnimation(animJump);
+marioCharacter.getComponent(ƒ.ComponentTransform).mtxLocal.translateY(-ƒ.Loop.timeFrameGame/1000 * marioSpeed);
+
+
+}
+    viewport.draw();
   }
 
+  async function actionHandler(_event: Event): Promise<void> {
+    let animWalk: ƒ.TextureImage = new ƒ.TextureImage();
+    await animWalk.load("Images/Mario/pngwing.com.png");
+    let walkAnimation: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, animWalk);
 
-  
+    animWalkRight = new ƒAid.SpriteSheetAnimation("walk", walkAnimation);
+    animWalkRight.generateByGrid(
+      ƒ.Rectangle.GET(0, 0, 223, 445),
+      3,
+      300,
+      ƒ.ORIGIN2D.BOTTOMCENTER,
+      ƒ.Vector2.X(223)
+    );
 
-  async function createWalkRightAnimation(): Promise<ƒAid.NodeSprite>{
-    
-    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
-    await imgSpriteSheet.load("Images/Mario/Mario_Walk.png");
-    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+    let imgIdle: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgIdle.load("Images/Mario/pngwing.com.png");
+    let idleAnimation: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgIdle);
 
-    let marioAnimation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("WalkRight", coat);
-    marioAnimation.generateByGrid(ƒ.Rectangle.GET(247, 1, 15, 28 ), 3, 30, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(14));
+    animIdle = new ƒAid.SpriteSheetAnimation("idle", idleAnimation);
+    animIdle.generateByGrid(
+      ƒ.Rectangle.GET(0, 0, 223, 445),
+      3,
+      300,
+      ƒ.ORIGIN2D.BOTTOMCENTER,
+      ƒ.Vector2.X(223)
+    );
 
-    marioSpriteNode = new ƒAid.NodeSprite("marioSprite");
+    let imgJump: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgJump.load("Images/Mario/pngwing.com.png");
+    let jumpAnimation: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgJump);
 
+    animJump = new ƒAid.SpriteSheetAnimation("idle", jumpAnimation);
+    animJump.generateByGrid(
+      ƒ.Rectangle.GET(0, 446, 223, 445),
+      3,
+      300,
+      ƒ.ORIGIN2D.BOTTOMCENTER,
+      ƒ.Vector2.X(223)
+    );
 
+    marioSpriteNode = new ƒAid.NodeSprite("Mario");
     marioSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
-    marioSpriteNode.setAnimation(marioAnimation);
-
+    marioSpriteNode.setAnimation(animIdle);
     marioSpriteNode.setFrameDirection(1);
     marioSpriteNode.mtxLocal.translateY(0.5);
-    marioSpriteNode.framerate = 4;
-    return marioSpriteNode;
+    marioSpriteNode.framerate = 15;
+    marioCharacter.removeAllChildren();
+    marioCharacter.addChild(marioSpriteNode);
   }
- /* async function createWalkLeftAnimation(): Promise<ƒAid.NodeSprite>{
-    
-    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
-    await imgSpriteSheet.load("Images/Mario/Mario_Walk.png");
-    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
-
-    let marioAnimation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation("WalkLeft", coat);
-    marioAnimation.generateByGrid(ƒ.Rectangle.GET(-158, -1, 15, 28 ), 3, 30, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(-28));
-
-    marioSpriteNode = new ƒAid.NodeSprite("marioSprite");
-
-
-    marioSpriteNode.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
-    marioSpriteNode.setAnimation(marioAnimation);
-
-    marioSpriteNode.setFrameDirection(1);
-    marioSpriteNode.mtxLocal.translateY(0.5);
-    marioSpriteNode.framerate = 4;
-    return marioSpriteNode;
-  }
-*/
 }
