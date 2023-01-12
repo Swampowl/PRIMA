8bda3cd0841ac599c1f6a0b9634f4a33a7aa3fbe
Florian Malitschenko <florian.malitschenko@outlook.de>
2023-01-12 22:17:33 +0100

    added left right walk

--- PixelVolley/Script/Build/Script.js
+++ PixelVolley/Script/Build/Script.js
@@ -39,22 +39,51 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
+    let branch;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let cmpCamera;
+    //blobs
+    let redBlob;
+    let blueBlob;
+    let characters;
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
         cmpCamera = viewport.camera;
+        branch = viewport.getBranch();
         ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         cmpCamera.mtxPivot.translate(new ƒ.Vector3(0, -1, 9));
         cmpCamera.mtxPivot.rotateY(180);
+        console.log(branch);
+        characters = branch.getChildrenByName("g_character")[0];
+        console.log(characters);
+        //characters.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(+1);
+        redBlob = characters.getChildrenByName("character_red")[0];
+        console.log(redBlob);
+        blueBlob = characters.getChildrenByName("character_blue")[0];
+        console.log(blueBlob);
     }
     function update(_event) {
         //  ƒ.Physics.simulate();  // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
+        movement();
+    }
+    function movement() {
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])) {
+            redBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.02);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])) {
+            redBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.015);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+            blueBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.015);
+        }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+            blueBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.02);
+        }
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- PixelVolley/Script/Source/Main.ts
+++ PixelVolley/Script/Source/Main.ts
@@ -1,24 +1,68 @@
 namespace Script {
   import ƒ = FudgeCore;
+
+  let branch: ƒ.Node;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   let cmpCamera: ƒ.ComponentCamera;
 
+  //blobs
+  let redBlob: ƒ.Node;
+  let blueBlob: ƒ.Node;
+  let characters: ƒ.Node;
+  
+
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-cmpCamera= viewport.camera;
+  cmpCamera= viewport.camera;
+  branch = viewport.getBranch();
+
+    
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     cmpCamera.mtxPivot.translate(new ƒ.Vector3(0, -1, 9));
     cmpCamera.mtxPivot.rotateY(180);
+    console.log(branch);
+
+    characters = branch.getChildrenByName("g_character")[0];
+    console.log(characters);
+    //characters.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(+1);
+    redBlob = characters.getChildrenByName("character_red")[0];
+    console.log(redBlob);
+    blueBlob = characters.getChildrenByName("character_blue")[0];
+    console.log(blueBlob);
+    
+
+    
+
   }
 
   function update(_event: Event): void {
   //  ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+    movement();
   }
+
+  function movement () {
+    if (ƒ.Keyboard.isPressedOne([ ƒ.KEYBOARD_CODE.D])) {
+      redBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.02);
+    }
+    if (ƒ.Keyboard.isPressedOne([ ƒ.KEYBOARD_CODE.A])) {
+        redBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.015);
+
+  }
+  if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
+    blueBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(0.015);
+  }
+  if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
+      blueBlob.getComponent(ƒ.ComponentTransform).mtxLocal.translateX(-0.02);
+
+  } 
+ }
 }
