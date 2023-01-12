07e640dec42ed8bf7226e209be930c97864701e2
Florian Malitschenko <florian.malitschenko@outlook.de>
2023-01-12 15:08:30 +0100

    addes pixel volley

---
+++ PixelVolley/Script/Source/Main.ts
@@ -0,0 +1,24 @@
+namespace Script {
+  import ƒ = FudgeCore;
+  ƒ.Debug.info("Main Program Template running!");
+
+  let viewport: ƒ.Viewport;
+  let cmpCamera: ƒ.ComponentCamera;
+
+  document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+  function start(_event: CustomEvent): void {
+    viewport = _event.detail;
+cmpCamera= viewport.camera;
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    cmpCamera.mtxPivot.translate(new ƒ.Vector3(0, -1, 9));
+    cmpCamera.mtxPivot.rotateY(180);
+  }
+
+  function update(_event: Event): void {
+    // ƒ.Physics.simulate();  // if physics is included and used
+    viewport.draw();
+    ƒ.AudioManager.default.update();
+  }
+}
