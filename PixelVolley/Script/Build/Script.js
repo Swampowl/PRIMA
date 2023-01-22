8cd396a19a544fa70b4d5e4d73c270fe5a554157
Florian Malitschenko <malitschenko@outlook.com>
2023-01-22 14:39:28 +0200

    this is now what it is

--- PixelVolley/Script/Build/Script.js
+++ PixelVolley/Script/Build/Script.js
@@ -14,21 +14,21 @@ var Script;
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
             // Listen to this component being added to or removed from a node
-            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
-            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
         }
         // Activate the functions of this component as response to events
         hndEvent = (_event) => {
             switch (_event.type) {
-                case "componentAdd" /* COMPONENT_ADD */:
+                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
                     ƒ.Debug.log(this.message, this.node);
                     break;
-                case "componentRemove" /* COMPONENT_REMOVE */:
-                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
-                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
                     break;
-                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
                     // if deserialized the node is now fully reconstructed and access to all its components and children is possible
                     break;
             }
@@ -39,6 +39,7 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     let branch;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
@@ -57,29 +58,42 @@ var Script;
     let wall_right_rigid;
     let wall_left;
     let wall_left_rigid;
+    let posBall_rigid;
+    let pos_blueBlob_rigid;
+    let pos_redBlob_rigid;
+    let animationBlue;
+    let animationRed;
     document.addEventListener("interactiveViewportStarted", start);
-    function start(_event) {
+    async function start(_event) {
         viewport = _event.detail;
         cmpCamera = viewport.camera;
         branch = viewport.getBranch();
         viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         //start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         cmpCamera.mtxPivot.translate(new ƒ.Vector3(0, -1, 10));
         cmpCamera.mtxPivot.rotateY(180);
         console.log(branch);
         characters = branch.getChildrenByName("g_character")[0];
-        console.log(characters);
+        //console.log(characters);
         //char //red
         redBlob = characters.getChildrenByName("character_red")[0];
-        console.log(redBlob);
+        // console.log(redBlob);
         redBlob_rigid = redBlob.getComponent(ƒ.ComponentRigidbody);
         redBlob_rigid.effectRotation = new ƒ.Vector3(0, 0, 0);
+        redBlob.addChild(createNewSpriteNode());
+        animationRed = await buildAnimation("Images/red_blobb.png", "redBlobb", 2, 0, 28, 32, 4, 31);
+        const spriteRed = redBlob.getChildrenByName("Sprite")[0];
+        spriteRed.setAnimation(animationRed);
         //char //blue
         blueBlob = characters.getChildrenByName("character_blue")[0];
-        console.log(blueBlob);
+        // console.log(blueBlob);
         blueBlob_rigid = blueBlob.getComponent(ƒ.ComponentRigidbody);
         blueBlob_rigid.effectRotation = new ƒ.Vector3(0, 0, 0);
+        blueBlob.addChild(createNewSpriteNode());
+        animationBlue = await buildAnimation("Images/blue_blobb.png", "blueBlobb", 2, 1, 31, 30, 5, 31);
+        const spriteBlue = blueBlob.getChildrenByName("Sprite")[0];
+        spriteBlue.setAnimation(animationBlue);
         //ball
         ball = branch.getChildrenByName("ball")[0];
         ball_rigid = ball.getComponent(ƒ.ComponentRigidbody);
@@ -99,44 +113,73 @@ var Script;
         ƒ.Loop.start();
     }
     function update(_event) {
-        let posBall_rigid;
-        let pos_blueBlob_rigid;
         ƒ.Physics.simulate();
         viewport.draw();
         ƒ.AudioManager.default.update();
         movement();
-        //console.log(ball.mtxLocal.translation.toString())
-        console.log(blueBlob.mtxLocal.translation.toString());
+        // console.log(ball.mtxLocal.translation.toString())
         posBall_rigid = ball_rigid.getPosition();
         posBall_rigid.z = 3;
-        // pos_blueBlob_rigid = blueBlob.getPos
-        // blueBlob_rigid.z =3 ;
+        pos_redBlob_rigid = redBlob_rigid.getPosition();
+        pos_blueBlob_rigid = blueBlob_rigid.getPosition();
+        //   console.log(posBall_rigid);
         ball_rigid.setPosition(posBall_rigid);
+        console.log(pos_blueBlob_rigid.y);
     }
     function movement() {
+        //redBlobb
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])) {
-            redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(+10, 0, 0));
+            redBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyForce(new ƒ.Vector3(+10, 0, 0));
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])) {
-            redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(-7.5, 0, 0));
+            redBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyForce(new ƒ.Vector3(-7.5, 0, 0));
         }
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W]) &&
+            pos_redBlob_rigid.y < -1.8) {
+            redBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyLinearImpulse(new ƒ.Vector3(0, .5, 0));
+        }
+        //blueBlobb
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
-            blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(-10, 0, 0));
+            blueBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyForce(new ƒ.Vector3(-10, 0, 0));
         }
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
-            blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(7.5, 0, 0));
+            blueBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyForce(new ƒ.Vector3(7.5, 0, 0));
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W])) {
-            redBlob.getComponent(ƒ.ComponentRigidbody).applyImpulseAtPoint(new ƒ.Vector3(0, 1, 0));
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP]) &&
+            pos_blueBlob_rigid.y < -1.8) {
+            blueBlob
+                .getComponent(ƒ.ComponentRigidbody)
+                .applyLinearImpulse(new ƒ.Vector3(0, .5, 0));
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP])) {
-            blueBlob.getComponent(ƒ.ComponentRigidbody).applyImpulseAtPoint(new ƒ.Vector3(0, 1, 0));
-        }
-        else {
-            blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(0, 0, 0));
-            redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(0, 0, 0));
-            redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(new ƒ.Vector3(0, -2, 0));
-        }
+    }
+    async function buildAnimation(path, name, startX, startY, width, height, frames, distanceBetweenSprites) {
+        let coat = await loadTextureFromSpriteSheet(path);
+        let animation = new ƒAid.SpriteSheetAnimation(name, coat);
+        animation.generateByGrid(ƒ.Rectangle.GET(startX, startY, width, height), frames, 30, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(frames > 0 ? distanceBetweenSprites : 0));
+        return animation;
+    }
+    async function loadTextureFromSpriteSheet(pathSpriteSheet) {
+        let imgSpriteSheet = new ƒ.TextureImage();
+        await imgSpriteSheet.load(pathSpriteSheet);
+        let coat = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+        return coat;
+    }
+    function createNewSpriteNode() {
+        let spriteNode = new ƒAid.NodeSprite("Sprite");
+        spriteNode.addComponent(new ƒ.ComponentTransform());
+        spriteNode.mtxLocal.translateY(-0.25);
+        spriteNode.mtxLocal.scale(new ƒ.Vector3(0.7, 0.7, 1));
+        return spriteNode;
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- PixelVolley/Script/Source/Main.ts
+++ PixelVolley/Script/Source/Main.ts
@@ -1,6 +1,6 @@
 namespace Script {
   import ƒ = FudgeCore;
-
+  import ƒAid = FudgeAid;
 
   let branch: ƒ.Node;
   ƒ.Debug.info("Main Program Template running!");
@@ -22,19 +22,24 @@ namespace Script {
   let wall_right_rigid: ƒ.ComponentRigidbody;
   let wall_left: ƒ.Node;
   let wall_left_rigid: ƒ.ComponentRigidbody;
+  let posBall_rigid: ƒ.Vector3;
+  let pos_blueBlob_rigid: ƒ.Vector3;
+  let pos_redBlob_rigid: ƒ.Vector3;
+  let animationBlue: ƒAid.SpriteSheetAnimation;
+  let animationRed: ƒAid.SpriteSheetAnimation;
 
 
+  document.addEventListener(
+    "interactiveViewportStarted",
+    <EventListener>(<unknown>start)
+  );
 
-  document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
-  function start(_event: CustomEvent): void {
+  async function start(_event: CustomEvent):  Promise<void>  {
     viewport = _event.detail;
     cmpCamera = viewport.camera;
     branch = viewport.getBranch();
 
     viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.JOINTS_AND_COLLIDER;
-
-
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     //start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -42,17 +47,30 @@ namespace Script {
     cmpCamera.mtxPivot.rotateY(180);
     console.log(branch);
     characters = branch.getChildrenByName("g_character")[0];
-    console.log(characters);
+    //console.log(characters);
+
     //char //red
     redBlob = characters.getChildrenByName("character_red")[0];
-    console.log(redBlob);
+    // console.log(redBlob);
     redBlob_rigid = redBlob.getComponent(ƒ.ComponentRigidbody);
     redBlob_rigid.effectRotation = new ƒ.Vector3(0, 0, 0);
+    redBlob.addChild(createNewSpriteNode());
+    animationRed = await buildAnimation("Images/red_blobb.png", "redBlobb", 2, 0, 28, 32, 4, 31);
+    const spriteRed = redBlob.getChildrenByName("Sprite")[0] as ƒAid.NodeSprite;
+      spriteRed.setAnimation(animationRed);
+
+
     //char //blue
     blueBlob = characters.getChildrenByName("character_blue")[0];
-    console.log(blueBlob);
+    // console.log(blueBlob);
     blueBlob_rigid = blueBlob.getComponent(ƒ.ComponentRigidbody);
     blueBlob_rigid.effectRotation = new ƒ.Vector3(0, 0, 0);
+    blueBlob.addChild(createNewSpriteNode());
+    animationBlue = await buildAnimation("Images/blue_blobb.png", "blueBlobb", 2, 1, 31, 30, 5, 31);
+    const spriteBlue = blueBlob.getChildrenByName("Sprite")[0] as ƒAid.NodeSprite;
+    spriteBlue.setAnimation(animationBlue);
+
+
     //ball
     ball = branch.getChildrenByName("ball")[0];
     ball_rigid = ball.getComponent(ƒ.ComponentRigidbody);
@@ -74,68 +92,103 @@ namespace Script {
     wall_right_rigid.effectRotation = new ƒ.Vector3(0, 0, 0);
 
     ƒ.Loop.start();
-
   }
 
   function update(_event: Event): void {
-    let posBall_rigid: ƒ.Vector3;
-    let pos_blueBlob_rigid: ƒ.Vector3;
     ƒ.Physics.simulate();
     viewport.draw();
     ƒ.AudioManager.default.update();
     movement();
-    //console.log(ball.mtxLocal.translation.toString())
-    console.log(blueBlob.mtxLocal.translation.toString())
+    // console.log(ball.mtxLocal.translation.toString())
     posBall_rigid = ball_rigid.getPosition();
     posBall_rigid.z = 3;
 
-    // pos_blueBlob_rigid = blueBlob.getPos
-    // blueBlob_rigid.z =3 ;
+    pos_redBlob_rigid = redBlob_rigid.getPosition();
+    pos_blueBlob_rigid = blueBlob_rigid.getPosition();
+    //   console.log(posBall_rigid);
     ball_rigid.setPosition(posBall_rigid);
+    console.log(pos_blueBlob_rigid.y);
   }
 
   function movement() {
+    //redBlobb
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D])) {
-      redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(+10, 0, 0));
-
+      redBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyForce(new ƒ.Vector3(+10, 0, 0));
     }
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A])) {
-      redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(-7.5, 0, 0));
-
-
+      redBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyForce(new ƒ.Vector3(-7.5, 0, 0));
     }
+    if (
+      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W]) &&
+      pos_redBlob_rigid.y < -1.8
+    ) {
+      redBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyLinearImpulse(new ƒ.Vector3(0, .5, 0));
+    }
+    //blueBlobb
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT])) {
-      blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(-10, 0, 0));
-
-
+      blueBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyForce(new ƒ.Vector3(-10, 0, 0));
     }
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT])) {
-      blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(7.5, 0, 0));
-
+      blueBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyForce(new ƒ.Vector3(7.5, 0, 0));
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W])) {
-      redBlob.getComponent(ƒ.ComponentRigidbody).applyImpulseAtPoint(new ƒ.Vector3(0, 1
-        , 0));
+    if (
+      ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP]) &&
+      pos_blueBlob_rigid.y < -1.8
+    ) {
+      blueBlob
+        .getComponent(ƒ.ComponentRigidbody)
+        .applyLinearImpulse(new ƒ.Vector3(0, .5, 0));
     }
+  }
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP])) {
-      blueBlob.getComponent(ƒ.ComponentRigidbody).applyImpulseAtPoint(new ƒ.Vector3(0, 1
-        , 0));
-    }
+  async function buildAnimation(
+    path: string,
+    name: string,
+    startX: number,
+    startY: number,
+    width: number,
+    height: number,
+    frames: number,
+    distanceBetweenSprites: number
+  ): Promise<ƒAid.SpriteSheetAnimation> {
+    let coat: ƒ.CoatTextured = await loadTextureFromSpriteSheet(path);
+    let animation: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation(
+      name,
+      coat
+    );
+    animation.generateByGrid(
+      ƒ.Rectangle.GET(startX, startY, width, height),
+      frames,
+      30,
+      ƒ.ORIGIN2D.BOTTOMCENTER,
+      ƒ.Vector2.X(frames > 0 ? distanceBetweenSprites : 0)
+    );
+    return animation;
+  }
 
-    else {
-      blueBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(0, 0, 0));
-      redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(0, 0, 0));
-      redBlob.getComponent(ƒ.ComponentRigidbody).applyForce(
-        new ƒ.Vector3(0, -2, 0));
-
-    }
-
+  async function loadTextureFromSpriteSheet(
+    pathSpriteSheet: string
+  ): Promise<ƒ.CoatTextured> {
+    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgSpriteSheet.load(pathSpriteSheet);
+    let coat: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+    return coat;
+  }
+ function createNewSpriteNode( ): ƒAid.NodeSprite {
+    let spriteNode = new ƒAid.NodeSprite("Sprite");
+    spriteNode.addComponent(new ƒ.ComponentTransform());
+    spriteNode.mtxLocal.translateY(-0.25);
+    spriteNode.mtxLocal.scale(new ƒ.Vector3(0.7, 0.7, 1))
+    return spriteNode;
   }
 }
